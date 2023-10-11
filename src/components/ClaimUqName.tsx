5f599382d56338dc517586a1a8afd9c87aa76fc5
realisation <realisation@protonmail.com>
2023-10-11 17:05:35 -0400

    updated ClaimUqName to set register with the fisr websocket information

--- src/components/ClaimUqName.tsx
+++ src/components/ClaimUqName.tsx
@@ -1,12 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { hooks } from "../connectors/metamask";
-import { UqNFT__factory } from "../abis/types";
-import {
-  UQ_NFT_ADDRESSES,
-} from "../constants/addresses";
-import { toDNSWireFormat } from "../utils/dnsWire";
-import Loader from "./Loader";
+import { UqNFT__factory, QNSRegistry__factory } from "../abis/types";
 import { Link, useNavigate } from "react-router-dom";
+import { toDNSWireFormat } from "../utils/dnsWire";
+import { utils } from 'ethers';
+import { 
+  UQ_NFT_ADDRESSES, 
+  QNS_REGISTRY_ADDRESSES 
+} from "../constants/addresses";
+import EnterUqName from "./EnterUqName";
+import Loader from "./Loader";
 
 const {
   useChainId,
@@ -18,46 +21,64 @@ type ClaimUqNameProps = {
   setConfirmedUqName: React.Dispatch<React.SetStateAction<string>>
 }
 
-
 function ClaimUqName({ setConfirmedUqName }: ClaimUqNameProps) {
   let chainId = useChainId();
   let accounts = useAccounts();
   let provider = useProvider();
   let navigate = useNavigate();
-  let [name, setName] = useState('');
   let [isLoading, setIsLoading] = useState(false);
 
   if (!chainId) return <p>connect your wallet</p>
   if (!provider) return <p>idk whats wrong</p>
   if (!(chainId in UQ_NFT_ADDRESSES)) return <p>change networks</p>
-  let uqNftAddress = UQ_NFT_ADDRESSES[chainId];
-  let uqNft = UqNFT__factory.connect(uqNftAddress, provider.getSigner());
+
+  const uqNft = UqNFT__factory.connect(
+    UQ_NFT_ADDRESSES[chainId], provider.getSigner());
+
+  const qns = QNSRegistry__factory.connect(
+    QNS_REGISTRY_ADDRESSES[chainId], provider.getSigner());
+
+  const [name, setName] = useState('')
+  const [nameValidities, setNameValidities] = useState<string[]>([])
+  const enterUqNameProps = { name, setName, nameValidities, setNameValidities }
+
+  const [networkingKey, setNetworkingKey] = useState<string>('')
+  const [ipAddress, setIAddress] = useState<string>("")
+  const [port, setPort] = useState<number>(0)
+  const [routers, setRouters] = useState<string[]>([])
+
+  useEffect(() => {
+    (async () => {
+      const response = await fetch('/get-ws-info', { method: 'GET' })
+      const data = await response.json()
+      setNetworkingKey(data.networking_key)
+      setRouters(data.allowed_routers)
+    })
+  }, [])
 
   let handleRegister = async () => {
-    if (!name) {
-      window.alert('Please enter a name')
-      return false
-    }
 
-    if (name.length < 9) {
-        window.alert('Name must be at least 9 characters')
-        return false
-    }
+    const wsTx = await qns.populateTransaction.setWsRecord(
+        utils.namehash(`${name}.uq`),
+        '0x'+networkingKey,
+        ipAddress,
+        port,
+        routers.map(x => utils.namehash(x))
+    )
 
     const dnsFormat = toDNSWireFormat(`${name}.uq`);
-
-    // TODO handle transaction rejected in wallet
-
     const tx = await uqNft.register(
       dnsFormat,
-      accounts![0], // TODO let the user know that this address will be the owner
-      []
+      accounts![0], 
+      [wsTx.data!]
     )
+
     setIsLoading(true);
     await tx.wait();
     setIsLoading(false);
     setConfirmedUqName(`${name}.uq`);
     navigate("/set-password");
+
   }
 
   return (
@@ -75,19 +96,8 @@ function ClaimUqName({ setConfirmedUqName }: ClaimUqNameProps) {
           <div className="row" style={{margin: "0 0 1em"}}>
               <div style={{fontSize: "0.75em"}}>Address:</div>
               {accounts && <div id="current-address">{accounts[0]}</div>}
-            </div>
-          <div className="row">
-            <input
-              value={name}
-              onChange={(e) => setName(e.target.value)}
-              type="text"
-              minLength={9}
-              required
-              name="uq-name"
-              placeholder="e.g. my-uqname"
-            />
-            <div className="uq">.uq</div>
           </div>
+          <EnterUqName { ...enterUqNameProps } />
           <button
             onClick={handleRegister}
           >Register Uqname</button>
