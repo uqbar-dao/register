b31e746a8f485e3dc08659f0497355aa59da1d2e
realisation <realisation@protonmail.com>
2023-10-19 13:38:22 -0400

    simplified Reset component to just reset records and login if password is present from just prior attempted login or redirect to setPassword if user went straight to /reset

--- src/components/Reset.tsx
+++ src/components/Reset.tsx
@@ -20,11 +20,18 @@ const {
 
 type ResetProps = {
   direct: boolean,
+  key: string,
+  keyFileName: string,
+  pw: string,
+  reset: boolean,
+  uqName: string,
   setDirect: React.Dispatch<React.SetStateAction<boolean>>,
-  setConfirmedUqName: React.Dispatch<React.SetStateAction<string>>
+  setReset: React.Dispatch<React.SetStateAction<boolean>>,
+  setPw: React.Dispatch<React.SetStateAction<string>>,
+  setUqName: React.Dispatch<React.SetStateAction<string>>
 }
 
-function Reset({ direct, setDirect, setConfirmedUqName }: ResetProps) {
+function Reset({ direct, setDirect, key, keyFileName, pw, setReset, uqName, setUqName }: ResetProps) {
   const chainId = useChainId();
   const accounts = useAccounts();
   const provider = useProvider();
@@ -35,24 +42,11 @@ function Reset({ direct, setDirect, setConfirmedUqName }: ResetProps) {
   const [port, setPort] = useState<number>(0);
   const [routers, setRouters] = useState<string[]>([]);
 
-  const [name, setName] = useState<string>('');
+  const [name, setName] = useState<string>(uqName.slice(0,-3));
   const [nameVets, setNameVets] = useState<string[]>([]);
 
   const [uploadKey, setUploadKey] = useState<boolean>(false)
-  const [resetted, setResetted] = useState<boolean>(false);
   const [needKey, setNeedKey] = useState<boolean>(false);
-  const [key, setKey] = useState<string>('');
-  const [keyFileName, setKeyFileName] = useState<string>('');
-  const [keyName, setKeyName] = useState<string>('');
-  const [keyNetKey, setKeyNetKey] = useState<string>('');
-  const [keyErrs, setKeyErrs] = useState<string[]>([]);
-
-  const [aprioriDirect, setAprioriDirect] = useState<boolean>(false);
-
-  const [pw, setPw] = useState<string>('');
-  const [pw2, setPw2] = useState<string>('');
-  const [pwErr, setPwErr] = useState<string>('');
-  const [pwVet, setPwVet] = useState<string>('');
 
   const [loading, setLoading] = useState<boolean>(false);
 
@@ -65,14 +59,6 @@ function Reset({ direct, setDirect, setConfirmedUqName }: ResetProps) {
       setRouters(data.allowed_routers)
       setIpAddr(ipToNumber(data.ws_routing[0]))
       setPort(data.ws_routing[1])
-
-      // await qns.setWsRecord(
-      //   namehash("trebuchet.uq"),
-      //   hexlify(randomBytes(32)),
-      //   1,
-      //   1,
-      //   []
-      // )
 
       response = await fetch('/has-keyfile', { method: 'GET'})
       data = await response.json()
@@ -96,14 +82,11 @@ function Reset({ direct, setDirect, setConfirmedUqName }: ResetProps) {
 
     nameDebouncer.current = setTimeout(async () => {
 
-        if (name == "") {
-          setNameVets([])
-          return;
-        }
+
+        if (name == "") { setNameVets([]); return; }
 
         let index: number
         let vets = [...nameVets]
-        const kErrs = [...keyErrs]
 
         let normalized: string
         index = vets.indexOf(NAME_INVALID_PUNY)
@@ -116,13 +99,6 @@ function Reset({ direct, setDirect, setConfirmedUqName }: ResetProps) {
 
         // only check if name is valid punycode
         if (normalized! !== undefined) {
-
-          if (keyName != '') {
-            index = kErrs.indexOf(KEY_DIFFERENT_USERNAME)
-            if (keyName != normalized) {
-              if (index == -1) kErrs.push(KEY_DIFFERENT_USERNAME)
-            } else if (index != -1) kErrs.splice(index, 1)
-          } 
 
           index = vets.indexOf(NAME_URL)
           if (name != "" && !isValidDomain(normalized)) {
@@ -142,156 +118,46 @@ function Reset({ direct, setDirect, setConfirmedUqName }: ResetProps) {
             index = vets.indexOf(NAME_NOT_REGISTERED)
             if (index != -1) vets.splice(index, 1)
 
-            const wsRecords = await qns.ws(namehash(normalized))
-            if (keyNetKey != '') {
-              index = kErrs.indexOf(KEY_WRONG_NET_KEY)
-              if (keyNetKey != wsRecords.publicKey) {
-                if (index == -1) kErrs.push(KEY_WRONG_NET_KEY)
-              } else if (index != -1) {
-                kErrs.splice(index, 1)
-
-                if (wsRecords.ip == 0) {
-                  setAprioriDirect(false)
-                  setDirect(false)
-                } else {
-                  setAprioriDirect(true)
-                  setDirect(true)
-                }
-
-              }
-            }
-
           } catch {
 
             index = vets.indexOf(NAME_NOT_REGISTERED)
             if (index == -1) vets.push(NAME_NOT_REGISTERED)
 
           }
+
+          if (nameVets.length == 0) 
+            setUqName(normalized)
+
         }
 
         setNameVets(vets)
-        setKeyErrs(kErrs)
 
     }, 500)
 
   }, [name])
 
-  const pwDebouncer = useRef<NodeJS.Timeout | null>(null)
-  useEffect(() => {
-
-    if (pwDebouncer.current) 
-      clearTimeout(pwDebouncer.current);
-
-    pwDebouncer.current = setTimeout(async () => {
-
-      if (pw2 != "" && pw != pw2)
-        setPwErr("Passwords do not match")
-      else {
-        setPwErr("")
-        if (6 <= pw.length) {
-          handlePassword()
-        }
-      }
-
-    }, 500)
-
-  }, [pw, pw2])
-
-  const KEY_DIFFERENT_USERNAME = "Keyfile does not match username"
-  const KEY_WRONG_NET_KEY = "Keyfile does not match public key"
-
-  const WS_WRONG_IP = "IP Address does not match records"
-  const WS_BAD_ROUTERS = "Routers from records are offline"
-
-  const handlePassword = async () => {
-    try {
-      const response = await fetch('/vet-keyfile', { 
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ 
-          keyfile: key,
-          password: pw
-        })
-      })
-
-      setPwVet("")
-
-      const data = await response.json()
-
-      setKeyName(data.username)
-
-      const errs = [...keyErrs]
-
-      let index = errs.indexOf(KEY_DIFFERENT_USERNAME)
-      if (data.username != `${name}.uq`) {
-        if (index == -1) errs.push(KEY_DIFFERENT_USERNAME)
-      } else if (index != -1) errs.splice(index, 1)
-
-      const ws = await qns.ws(namehash(data.username))
-      setKeyNetKey(`0x${data.networking_key}`)
-
-      index = errs.indexOf(KEY_WRONG_NET_KEY)
-      if (ws.publicKey != '0x' + data.networking_key) {
-        if (index == -1) errs.push(KEY_WRONG_NET_KEY)
-      } else if (index != -1) errs.splice(index, 1)
-
-      index = errs.indexOf(WS_WRONG_IP)
-      if (ws.ip != 0 && ws.ip != ipAddr) {
-        if (index == -1) errs.push(WS_WRONG_IP)
-      } else if (index != -1) errs.splice(index, 1)
-
-      setKeyErrs(errs)
-
-    } catch {
-      setPwVet("Password is incorrect")
-    }
-  }
-
-  const handleKeyfile = async (e: any) => {
-    e.preventDefault()
-    const file = e.target.files[0]
-    if (!file) return
-    const reader = new FileReader()
-    reader.onloadend = () => {
-      setKeyFileName(file.name)
-      setKey(reader.result as string)
-    }
-    reader.readAsText(file)
-  }
-
-  const keyfileInputRef = useRef<HTMLInputElement>(null);
-
-  const handleKeyUploadClick = async (e: any) => {
-    e.preventDefault()
-    keyfileInputRef.current?.click()
-  }
-
   const handleLogin = async () => {
-
-    if (resetted || keyErrs.length == 0 || keyErrs.length == 0) {
 
     const response = await fetch('/boot', { 
       method: 'PUT',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ 
-        keyfile: resetted ? "" : key,
-        reset: resetted,
+        keyfile: "",
+        reset: true,
         password: pw,
         username: `${name}.uq`,
         direct
       })
     })
-
-    if (!needKey || resetted) {
-      const base64Keyfile = await response.json()
-      let blob = new Blob([base64Keyfile], {type: "text/plain;charset=utf-8"});
-      const url = window.URL.createObjectURL(blob)
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', `${name}.uq.keyfile`)
-      document.body.appendChild(link);
-      link.click();
-    }
+    
+    const base64Keyfile = await response.json()
+    let blob = new Blob([base64Keyfile], {type: "text/plain;charset=utf-8"});
+    const url = window.URL.createObjectURL(blob)
+    const link = document.createElement('a');
+    link.href = url;
+    link.setAttribute('download', `${name}.uq.keyfile`)
+    document.body.appendChild(link);
+    link.click();
 
     const interval = setInterval(async () => {
       const homepageResult = await fetch('/') 
@@ -301,14 +167,12 @@ function Reset({ direct, setDirect, setConfirmedUqName }: ResetProps) {
       }
     }, 2000);
 
-    }
-
   };
 
   const handleResetRecords = async (asDirect: boolean) => {
 
     const tx = await qns.setWsRecord(
-      namehash(`${name}.uq`),
+      namehash(uqName),
       `0x${networkingKey}`,
       asDirect ? ipAddr : 0,
       asDirect ? port : 0,
@@ -319,9 +183,13 @@ function Reset({ direct, setDirect, setConfirmedUqName }: ResetProps) {
 
     await tx.wait();
 
-    setResetted(true);
-    setLoading(false);
-    setDirect(asDirect);
+    if (pw) handleLogin();
+    else {
+      setReset(true);
+      setLoading(false);
+      setDirect(asDirect);
+      navigate('/set-password');
+    }
 
   }
 
@@ -371,87 +239,12 @@ function Reset({ direct, setDirect, setConfirmedUqName }: ResetProps) {
         { nameVets.map((x,i) => <span key={i} className="name-err">{x}</span>) }
       </div>
 
-      <div className="login-row row">
-        2. Choose Keyfile
-        <br/>
-      </div>
+      <label htmlFor="direct">
+        Reset info as a direct node (only do this if you are hosting your node somewhere stable)
+      </label>
+      <input type="checkbox" id="direct" name="direct" checked={direct} onChange={(e) => setDirect(e.target.checked)}/>
 
-      <div className="row" style={{margin: ".5em", opacity: needKey ? .5 : 1}}> 
-        <label> 
-          <input disabled={needKey} type="checkbox" checked={!uploadKey} onChange={flipUploadKey} />
-          { needKey ? "No" : "Use" } Existing Keyfile 
-        </label>
-      </div>
-
-      <div style={{margin: ".5em", display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', opacity: uploadKey ?  1 : .5}}>
-        <div className="row"> 
-          <label>
-            <input type="checkbox" checked={uploadKey} onChange={flipUploadKey} />
-            Upload Keyfile
-          </label>
-        </div>
-        <button style={{width: "50%", fontSize: "65%" }} onClick={handleKeyUploadClick}> 
-          { keyFileName ? "Change" : "Upload" } Keyfile
-        </button>
-        <p style={{opacity: !keyFileName ? .5 : 1 }}> { keyFileName ? keyFileName : ".keyfile"} </p>
-        <input ref={keyfileInputRef} style={{display:"none"}} type="file" onChange={handleKeyfile} />
-      </div>
-
-      <div className="login-row row">
-        3. Enter Password
-        <br/>
-      </div>
-
-      <div className="row">
-        <div className="row label-row">
-          <label htmlFor="password">Enter Password</label>
-        </div>
-        <input
-          type="password"
-          id="password"
-          required
-          minLength={6}
-          name="password"
-          placeholder="Min 6 characters"
-          value={pw}
-          onChange={(e) => setPw(e.target.value)}
-        />
-      </div>
-
-      <div className="row">
-        <div className="row label-row">
-          <label htmlFor="confirm-password">Confirm Password</label>
-        </div>
-        <input
-          type="password"
-          id="confirm-password"
-          required minLength={6}
-          name="confirm-password"
-          placeholder="Min 6 characters"
-          value={pw2}
-          onChange={(e) => setPw2(e.target.value)}
-        />
-      </div>
-
-      { pwErr ??  <div className="row"> <p style={{color:"red"}}> {pwErr}</p> </div> }
-      { pwVet ??  <div className="row"> <p style={{color:"red"}}> {pwVet}</p> </div> }
-
-      <div className="login-row row">
-        4. Overview
-      </div>
-
-      <div className="col">
-        { keyErrs.map((x,i) => <span key={i} className="key-err">{x}</span>) }
-        { (keyErrs.length > 1 || (keyErrs.length == 1 && keyErrs.indexOf(KEY_DIFFERENT_USERNAME) == -1)) ?
-          <>
-            <button onClick={()=>handleResetRecords(false)}> Reset </button>
-            <input type="checkbox" checked={direct} onChange={()=>setDirect(!direct)} />  
-            as direct node
-          </> : ""
-        }
-      </div>
-
-      <button onClick={handleLogin}> Submit </button>
+      <button onClick={()=>handleResetRecords(direct)}> Reset Networking Keys </button>
 
       </>
     }
